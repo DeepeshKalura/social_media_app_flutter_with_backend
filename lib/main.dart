9b4dd392a1ff34d3b8cdf996361596970cdca879
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-16 12:37:09 +0530

    change blue

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Social Media App'),
     );
